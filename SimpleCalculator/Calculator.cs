e822a239816f1f058270a7a0f092c41c5f5c206c
ethan moodley <ethanshaunmoodley@gmail.com>
2024-05-09 22:22:08 +0100

    Add project files.

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ST10351721
+//Ethan Shaun Moodley 
+//PROG ICE Task 4
+
+namespace SimpleCalculator
+{
+
+    public delegate void CalculationEventHandler(string operation, double result);
+
+    public class Calculator
+    {
+
+       
+        public event CalculationEventHandler CalculationPerformed;
+       
+        public double Add(double num1 , double num2)
+        {
+            double result = num1 + num2;
+
+            OnCalculationPerformed("Addition", result);
+            return result;
+        }
+
+        public double Subtract(double num1,double num2)
+        {
+            double result = num1 - num2;
+
+            OnCalculationPerformed("Subtraction", result);
+            return result;
+        }
+
+        public double Multiply(double num1 ,double num2)
+        {
+            double result = num1 * num2;
+
+            OnCalculationPerformed("Multiplication" , result);
+            return result;
+        }
+
+        public double Divide(double num1,double num2)
+        {
+            if(num1 == 0 || num2 == 0 )
+            {
+                //Error checking is value will become undefined
+                Console.WriteLine("Error: Division by zero.");
+                return double.NaN;
+            }
+
+            double result = num1 / num2;
+            OnCalculationPerformed("Division", result);
+            return result;
+        }
+
+        protected virtual void OnCalculationPerformed(string operation, double result)
+        {
+            CalculationPerformed?.Invoke(operation, result);
+        }
+
+
+
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator CalcObj = new Calculator();
+
+            CalcObj.CalculationPerformed += (operation, result) =>
+            {
+                Console.WriteLine($"Operation:{operation}, Result: {result}");
+            };
+
+            Console.WriteLine("Enter two numbers:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose operation (+ , - , * , /):");
+            char UserOption = Convert.ToChar(Console.ReadLine());
+
+            double UserOutput;
+
+            //Switch , case statement gets char value and therefore assignes appropriate calculated value in result
+            switch (UserOption)
+            {
+                case '+':
+                    UserOutput = CalcObj.Add(num1 , num2);
+                    break;
+                case '-':
+                    UserOutput = CalcObj.Subtract(num1, num2);
+                    break;
+                case '*':
+                    UserOutput = CalcObj.Multiply(num1, num2);
+                    break;
+                case '/':
+                    UserOutput = CalcObj.Divide(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    UserOutput = double.NaN;
+                    break;
+            }
+
+            Console.WriteLine($"Result: {UserOutput}");
+
+
+
+
+
+        }
+    }
+}
